8b25c6d26618ffb569767931660036100fa307a1
pthomas2001 <penzestamas8@gmail.com>
2020-09-25 15:40:53 +0200

    directions

---
+++ scripts.js
@@ -0,0 +1,16 @@
+let color = "purple"
+let number = 10
+let word = "cool"
+if (color === "purple") {
+    $('.red').css("background", "purple")
+}
+if (number > 100) {
+    $('.yellow').text("whoah, that's a big number.")
+} else {
+    $('.yellow').text("just a regular number, please.")
+}
+if (word === "cool") {
+    $('.green').text("Power of DOM")
+} else {
+    $('.blue').text("Power of DOM")
+}
